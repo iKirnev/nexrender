4a753b43c5c2ba6f340b6e66dafd16d50ca5bfa5
Ivan <ivan.kirnev@gmail.com>
2019-06-08 00:38:29 +0300

    commenting unnecessary logs

--- packages/nexrender-server/src/routes/jobs-fetch.js
+++ packages/nexrender-server/src/routes/jobs-fetch.js
@@ -3,10 +3,10 @@ const { fetchJob, fetchJobs }  = require('../helpers/mongo')
 
 module.exports = async (req, res) => {
     if (req.params.uid) {
-        console.log(`fetching job ${req.params.uid}`)
+        //console.log(`fetching job ${req.params.uid}`)
         send(res, 200, await fetchJob(req.params.uid))
     } else {
-        console.log(`fetching list of all jobs`)
+        //console.log(`fetching list of all jobs`)
         send(res, 200, await fetchJobs())
     }
 }
